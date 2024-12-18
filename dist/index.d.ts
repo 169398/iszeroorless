6f7665b442de121eec8608cbbba2eeb83b95c331
idris kulubi <137810838+169398@users.noreply.github.com>
2024-12-18 21:02:35 +0300

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function isZeroOrLess(num: number): boolean;
-export default isZeroOrLess;
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isZeroOrLess = isZeroOrLess;
-function isZeroOrLess(num) {
-    return num <= 0;
-}
-exports.default = isZeroOrLess;
